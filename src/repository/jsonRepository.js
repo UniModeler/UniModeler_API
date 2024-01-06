fa280d770cffa82984da1f34c40f981fb1463035
igao <nascimentoigor1311@gmail.com>
2024-01-06 14:06:54 -0300

    verificação de tipo 'date' aprimorada

--- src/repository/jsonRepository.js
+++ src/repository/jsonRepository.js
@@ -36,26 +36,34 @@ export function estruturaObjeto(jsString) {
             let type = dataType(property.value);
             let nestedAttributes;
             let description = thereIsComment(property.loc);
+            let example;
 
             if (type === 'object') {
                 nestedAttributes = infoObjectNode(property.value);
-            }
-
-            if (type === 'array_object') {
+            } 
+            else if (type === 'array_object') {
                 nestedAttributes = infoObjectNode(property.value.elements[0])
+            }               
+            else {
+                example = String(property.value.value);
             }
 
             let infoField = {
                 name: name,
                 type: type,
-            };
+            }
 
             if (nestedAttributes) {
                 infoField.attributes = nestedAttributes;
-            };
+            }
 
             if (description) {
                 infoField.description = description;
+            }
+
+            if (example) {
+                infoField.example = example;
+                console.log(infoField.example);
             }
 
             verifyName(infoField, property);
@@ -72,17 +80,19 @@ export function estruturaObjeto(jsString) {
         if (propertyValue.type === 'Literal') {
             type = typeof (propertyValue.value);
 
+            let dataRegExp = /(?:(?:1[6-9]|[2-9]\d)?\d{2})(?:(?:(\/|-|\.)(?:0?[13578]|1[02])\1(?:31))|(?:(\/|-|\.)(?:0?[13-9]|1[0-2])\2(?:29|30)))$|^(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00)))(\/|-|\.)0?2\3(?:29)$|^(?:(?:1[6-9]|[2-9]\d)?\d{2})(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:0?[1-9]|1\d|2[0-8])/
+
             if (type === 'number') {
                 type = Number.isInteger(propertyValue.value) ? 'int' : 'double';
             }
 
-            if (type === 'string' && new Date(propertyValue.value).toString() !== 'Invalid Date') {
-                
+            if (type === 'string' && dataRegExp.test(propertyValue.value)) {
                 if(/[0-2][0-9]:[0-5][0-9]/.test(propertyValue.value)) {
                     type = 'datetime';
                 } else {
                     type = 'date';
                 }   
+
             } else if (/^[0-2][0-9]:[0-5][0-9]$/.test(propertyValue.value)) {
                 type = 'time';
             }
@@ -131,11 +141,13 @@ export function estruturaObjeto(jsString) {
             infoField.key = 'foreign key';
             infoField.name = infoField.name.replace('#', '');
             infoField.references = property.value.value;
+            infoField.example = undefined;
         }
 
         if (firstChar === '_') {
             infoField.name = infoField.name.replace('_', '');
             infoField.type = `'${String(property.value.value)}'`;
+            infoField.example = undefined;
         }
     }
 }
