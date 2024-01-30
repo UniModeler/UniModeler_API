8eb4bf1d01a08bc8a76a5fce85f3e4732921c54e
igao <nascimentoigor1311@gmail.com>
2024-01-30 16:24:31 -0300

    .

--- src/infra/repository/projects/index.images.js
+++ src/infra/repository/projects/index.images.js
@@ -4,9 +4,7 @@ import { connect } from "../base/connection.js";
 const [collection] = connect('projects');
 
 export async function addImage(projectId, imagePath) {
-
-    console.log('a');
-
+    
     let r = await collection.updateOne({
         _id: new ObjectId(projectId)
     }, {
