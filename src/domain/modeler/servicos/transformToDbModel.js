c78d317ed5f674a7fe3880e739363fb7b4e15d5e
Igor <nascimentoigor1311@gmail.com>
2024-03-13 20:43:23 -0300

    tratamento de erro no endpoint do model

--- src/domain/modeler/servicos/transformToDbModel.js
+++ src/domain/modeler/servicos/transformToDbModel.js
@@ -1,5 +1,9 @@
 import { estruturaObjeto } from '#infra/ast/DbModel'
 
 export function transformToDbModel(code) {
-  return estruturaObjeto(code);
+  try {
+    return estruturaObjeto(code);
+  } catch (error) {
+    throw new UniModelerError('Syntax error: line ' + error.loc.line);
+  }
 }
